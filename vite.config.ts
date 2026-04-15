a5e1f8f3c3c020269ed615567f351b592080a548
Eshan <erdegamwar@gmail.com>
2026-04-15 14:03:38 -0400

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,34 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig, loadEnv} from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig(({mode}) => {
+export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
+  const repository = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? '';
+  const isUserOrOrgPagesRepo = repository.endsWith('.github.io');
+
+  const base = process.env.GITHUB_ACTIONS
+    ? isUserOrOrgPagesRepo
+      ? '/'
+      : `/${repository}/`
+    : '/';
+
   return {
+    base,
+
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
+    
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
       },
     },
+
     server: {
-      // HMR is disabled in AI Studio via DISABLE_HMR env var.
-      // Do not modifyâfile watching is disabled to prevent flickering during agent edits.
       hmr: process.env.DISABLE_HMR !== 'true',
     },
   };
