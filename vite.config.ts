ab72f0706559581b7dfdfabd02d35eb5dc4fe29c
Eshan <erdegamwar@gmail.com>
2026-04-15 14:07:15 -0400

    npm updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, '.', '');
+export default defineConfig(() => {
   const repository = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? '';
   const isUserOrOrgPagesRepo = repository.endsWith('.github.io');
 
@@ -18,9 +17,6 @@ export default defineConfig(({ mode }) => {
     base,
 
     plugins: [react(), tailwindcss()],
-    define: {
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-    },
     
     resolve: {
       alias: {
